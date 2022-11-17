34fd56b193a10b8b01fa3fb1ed961bcf20806388
b0yage <christivnvertiz@gmail.com>
2022-11-17 08:35:11 -0600

    fixed markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `## License
+  ![Github license](https://img.shields.io/badge/license-${license}-yellow.svg)`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return `
+    MIT License, Copyright (C) 2021
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE. `;
+  }
+  if (license === "None") {
+    return "Choose a license: [https://choosealicense.com/]";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,6 +39,30 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ${renderLicenseBadge(data.license)}
+${renderLicenseLink(data.license)}
+## Description
+${data.description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribute](#contribution)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+${data.installation}
+## Usage 
+${data.usage}
+## Contribute
+${data.contribution}
+## Tests
+${data.test}
+## Questions
+For questions checkout my GitHub account (https://github.com/${
+    data.GitHub
+  }) or email me at ${data.email} 
+
 
 `;
 }
